ef7a667af517d700130e5264715ccf10a26a34f8
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-22 10:28:22 -0500

    Aggregate Path *

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+//*Excepción - regresamos el index
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 //*Escuchar peticiones
 app.listen( process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
